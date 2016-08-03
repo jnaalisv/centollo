123125b7318f707f507c08636376078280c5258a
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2016-08-03 22:18:36 +0300

    change log level to clear up logs

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -8,10 +8,10 @@ root(INFO, ["STDOUT"])
 
 logger("centollo", INFO)
 logger("org.springframework", INFO)
-logger("org.springframework.jdbc.datasource.DataSourceTransactionManager", DEBUG)
+logger("org.springframework.jdbc.datasource.DataSourceTransactionManager", INFO)
 
 logger("org.hibernate", INFO)
-logger("org.springframework.orm.hibernate5.HibernateTransactionManager", DEBUG)
+logger("org.springframework.orm.hibernate5.HibernateTransactionManager", INFO)
 
 logger("org.hibernate.tool.hbm2ddl.SchemaExport", INFO)
 logger("org.hibernate.SQL", INFO)
